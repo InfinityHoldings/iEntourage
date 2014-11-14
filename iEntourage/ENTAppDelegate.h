9134c9b7fe31d376f700676ef1956550e41f82fd
KeonW <keontwilliams@gmail.com>
2014-11-14 18:20:58 -0500

    Initial Commit

---
+++ iEntourage/ENTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ENTAppDelegate.h
+//  iEntourage
+//
+//  Created by King Keon on 11/14/14.
+//  Copyright (c) 2014 King Keon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ENTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iEntourage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iEntourage
+//
+//  Created by King Keon on 11/14/14.
+//  Copyright (c) 2014 King Keon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ENTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ENTAppDelegate class]));
+    }
+}
